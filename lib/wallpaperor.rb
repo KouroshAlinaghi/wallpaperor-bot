ab3b0f3f7ca6bcc3bbf54efc3bd1788c3775cf9e
KouroshAlinaghi <kouroshalinaghi@gmail.com>
2020-09-24 16:05:47 +0330

    0

--- lib/wallpaperor.rb
+++ lib/wallpaperor.rb
@@ -9,22 +9,20 @@ tg_token = ENV["TG_TOKEN"]
 Telegram::Bot::Client.run(tg_token) do |bot|
   bot.listen do |msg|
     if msg.from && msg.from.id == ENV["MY_ID"]
-#      case msg.text
-#      when /^https:\/\/unsplash/
-#        photo_path = Helpers.get_unsplash_photo_path_and_caption(msg)[0]
-#        caption = Helpers.get_unsplash_photo_path_and_caption(msg)[1]
-#        send = true
-#      when /^https:\/\/www.pexels.com/
-#        photo_path = Helpers.get_pexels_photo_path_and_caption(msg)[0]
-#        caption = Helpers.get_pexels_photo_path_and_caption(msg)[1]
-#        send = true
-#      else
+      case msg.text
+      when /^https:\/\/unsplash/
+        photo_path = Helpers.get_unsplash_photo_path_and_caption(msg)[0]
+        caption = Helpers.get_unsplash_photo_path_and_caption(msg)[1]
+        send = true
+      when /^https:\/\/www.pexels.com/
+        photo_path = Helpers.get_pexels_photo_path_and_caption(msg)[0]
+        caption = Helpers.get_pexels_photo_path_and_caption(msg)[1]
+        send = true
+      else
         bot.api.send_message(chat_id: msg.chat.id, text: "Invalid URL")
-#        send = false
-#      end
+        send = false
+      end
 #      bot.api.send_document(chat_id: "@ilovecloudstorage", document: photo_path, caption: caption, parse_mode: "Markdown") if send
     end
   end
 end
-
-
